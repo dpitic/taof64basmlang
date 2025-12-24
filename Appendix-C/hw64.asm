dd9b831f34789b4071a414f261e44d38101a9b7c
Dan Pitic <dpitic@gmail.com>
2025-12-24 14:07:40 +0800

    Initial code commit. Added Visual Studio ignores to local gitignore file.

---
+++ Appendix-C/hw64.asm
@@ -0,0 +1,48 @@
+include kernel32.lib
+
+		extrn	__imp_GetStdHandle:proc
+		extrn	__imp_WriteFile:proc
+
+		.CODE
+hwStr	byte	"Hello World!"
+hwLen	=		$ - hwStr
+
+main	PROC
+
+; On entry, stack is aligned at 8 mod 16. Set aside 8
+; bytes for "bytesWritten" ensures that calls in main have
+; their stack aligned to 16 bytes (8 mod 16 inside function).
+		lea		rbx, hwStr
+		sub		rsp, 8
+		mov		rdi, rsp		; Hold # of bytes written here
+
+; Note: must set aside 32 bytes (20h) for shadow registers for
+; parameters (just do this once for all functions).
+; Also, WriteFile has a 5th argument (which is NULL),
+; so we must set aside 8 bytes to hold that pointer (and
+; initialise it to zero). Finally, the stack muste always be
+; 16-byte aligned, so reserve another 8 bytes of storage
+; to ensure this.
+
+; Shadow storage for args (always 30h bytes).
+		sub		rsp, 30h
+
+; Handle = GetStdHandle(-11);
+; Single argument passed in ECX.
+; Handle returned in RAX.
+		mov		rcx, -11		; STD_OUTPUT
+		call	qword ptr __imp_GetStdHandle
+
+; WriteFile(handle, "Hello World!", 12, &bytesWritten, NULL);
+; Zero out (set to NULL) "LPOverlapped" argument:
+		mov		qword ptr [rsp + 4 * 8], 0	; 5th argument on stack
+
+		mov		r9, rdi			; Address of "bytesWritten" in R9
+		mov		r8d, hwLen		; Length of string to write in R8D
+		lea		rdx, hwStr		; Ptr to string data in RDX
+		mov		rcx, rax		; File handle passed in RCX
+		call	qword ptr __imp_WriteFile
+		add		rsp, 38h		; Clean up stack (30h + 8 bytes)
+		ret
+main	ENDP
+		END	main
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # taof64basmlang
-The Art of 64-bit Assembly Language
+The Art of 64-bit Assembly. Volume 1, x86-64 machine organisation and programming. Randall Hyde. 2022.
